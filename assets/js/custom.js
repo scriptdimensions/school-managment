c24bd4ae9acd0020d7db8325906223c8ea4d468c
scriptdimensions <scriptdimensions@gmail.com>
2024-06-30 18:09:12 +0100

    registration form

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -72,6 +72,21 @@ jQuery(document).ready(function () {
 			});
 			$('.sidebar .icons li ').eq(index).find('.tool').text(item.find('.text').text());
 		})
+
+		//
+
+		document.getElementById('imageUpload').addEventListener('change', function(event) {
+            const file = event.target.files[0];
+            if (file) {
+                const reader = new FileReader();
+                reader.onload = function(e) {
+                    const preview = document.getElementById('imagePreview');
+                    preview.src = e.target.result;
+                    preview.style.display = 'block';
+                };
+                reader.readAsDataURL(file);
+            }
+        });
 		//====
 	})(jQuery);
 });
