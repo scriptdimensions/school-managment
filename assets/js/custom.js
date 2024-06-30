caae4422ffe157aa574623445e0191c9b7599571
scriptdimensions <scriptdimensions@gmail.com>
2024-06-30 16:52:11 +0100

    sidear-header

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -37,30 +37,41 @@ jQuery(document).ready(function () {
 		function changetab(index) {
 			$('.sidebar .icons li').eq(index).addClass('active').siblings().removeClass('active');
 			$('.sidebar .item-bar li').eq(index).addClass('active').siblings().removeClass('active');
-			console.log(index)
 		}
 		$('.sidebar :is(.icon-bar .icons, .item-bar)').each(function () {
 			$(this).find('li').each(function (index) {
-				$(this).on('click', function (e) {
-					changetab(index)
+				$(this).find('.text').on('click', function (e) {
+					changetab(index);
+					$('.sidebar .item-bar li .sub').slideUp(200);
+					$('.sidebar .item-bar .item').removeClass('show');
 				});
 			});
 		})
 
 		$(document).on("click", function (e) {
-			if (!$(e.target).is(".sidebar") && !jQuery(e.target).is(".sidebar *")  && !jQuery(e.target).is(".menu-icon *")) {
-			  $(".sidebar").removeClass("active");
+			if (!$(e.target).is(".sidebar") && !jQuery(e.target).is(".sidebar *") && !jQuery(e.target).is(".menu-icon *")) {
+				$(".sidebar").removeClass("active");
+				$('.sidebar .item-bar li .sub').slideUp(200);
+				$('.sidebar .item-bar .item').removeClass('show');
 			}
 			if (!$(e.target).is(".head-sub") && !jQuery(e.target).is(".head-sub *")) {
 				$(".head-sub").removeClass("show");
-			  }
+			}
 		});
 
 		var nameUser = $('.profile-name h4').text().split(' ');
 		var firstLetters = nameUser.map(word => word.charAt(0)).join('');
 		$('.user-setting .icon').text(firstLetters)
-		
-	console.log(nameUser)
+
+		$('.sidebar .item-bar .item').each(function (index) {
+			var item = $(this);
+			var button = item.find('img');
+			button.on('click', function () {
+				item.find('.sub').toggle(200);
+				item.toggleClass('show');
+			});
+			$('.sidebar .icons li ').eq(index).find('.tool').text(item.find('.text').text());
+		})
 		//====
 	})(jQuery);
 });
