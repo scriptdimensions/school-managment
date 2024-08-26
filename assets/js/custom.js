035628aeba2a980691cd27f332c99bf1767fef62
scriptdimensions <scriptdimensions@gmail.com>
2024-08-26 18:16:04 +0500

    school managment

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -11,28 +11,297 @@ jQuery(document).ready(function () {
 		}
 
 
-		jQuery(window).on("scroll", function () {
-			if (jQuery(window).scrollTop()) {
-				jQuery('header').addClass('shadow');
-			}
-			else {
-				jQuery('header').removeClass('shadow');
-			}
-		});
 
 
-		$('.head-sub').each(function () {
-			let head = $(this),
-				icon = head.find('.icon');
-			icon.on('click', function () {
-				head.toggleClass('show').siblings().removeClass('show')
-			});
-		});
 
-		$('.menu-icon').on('click', function () {
+
+
+		$('.icon-wrap').on('click', function () {
 			$('.sidebar').toggleClass('active');
-			$('.s-navi .await').toggleClass('active');
+			$('.main-logo').toggleClass('sidebar-collapsed');
+			$('.main-content').toggleClass('side-active');
 		})
+		$('.sidebar .nav-item').each(function () {
+			let item = $(this);
+			item.find('.nav-link').on('click', function (e) {
+				e.preventDefault();
+				item.siblings().removeClass('active');
+				item.siblings().find('.sub').slideUp(200);
+				item.find('.sub').slideToggle(200);
+				item.toggleClass('active');
+			})
+		})
+		$(".counter .num").counterUp({ time: 3000 });
+		$('.navbar .navbar-item').each(function () {
+			let item = $(this),
+				button = item.find('.dropdown-toggle');
+			button.on('click', function () {
+				item.siblings().removeClass('show');
+				item.toggleClass('show');
+			})
+		})
+		$('#scrollUp').on('click', function () {
+			$('html, body').animate({ scrollTop: 0 }, 500); // 500 milliseconds for smooth scroll
+		});
+		
+		///
+		/*-------------------------------------
+			  Line Chart 
+		  -------------------------------------*/
+		if ($("#earning-line-chart").length) {
+
+			var lineChartData = {
+				labels: ["", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun", ""],
+				datasets: [{
+					data: [0, 5e4, 1e4, 5e4, 14e3, 7e4, 5e4, 75e3, 5e4],
+					backgroundColor: '#ff0000',
+					borderColor: '#ff0000',
+					borderWidth: 1,
+					pointRadius: 0,
+					pointBackgroundColor: '#ff0000',
+					pointBorderColor: '#ffffff',
+					pointHoverRadius: 6,
+					pointHoverBorderWidth: 3,
+					fill: 'origin',
+					label: "Total Collection"
+				},
+				{
+					data: [0, 3e4, 2e4, 6e4, 7e4, 5e4, 5e4, 9e4, 8e4],
+					backgroundColor: '#417dfc',
+					borderColor: '#417dfc',
+					borderWidth: 1,
+					pointRadius: 0,
+					pointBackgroundColor: '#304ffe',
+					pointBorderColor: '#ffffff',
+					pointHoverRadius: 6,
+					pointHoverBorderWidth: 3,
+					fill: 'origin',
+					label: "Fees Collection"
+				}
+				]
+			};
+			var lineChartOptions = {
+				responsive: true,
+				maintainAspectRatio: false,
+				animation: {
+					duration: 2000
+				},
+				scales: {
+
+					xAxes: [{
+						display: true,
+						ticks: {
+							display: true,
+							fontColor: "#222222",
+							fontSize: 16,
+							padding: 20
+						},
+						gridLines: {
+							display: true,
+							drawBorder: true,
+							color: '#cccccc',
+							borderDash: [5, 5]
+						}
+					}],
+					yAxes: [{
+						display: true,
+						ticks: {
+							display: true,
+							autoSkip: true,
+							maxRotation: 0,
+							fontColor: "#646464",
+							fontSize: 16,
+							stepSize: 25000,
+							padding: 20,
+							callback: function (value) {
+								var ranges = [{
+									divider: 1e6,
+									suffix: 'M'
+								},
+								{
+									divider: 1e3,
+									suffix: 'k'
+								}
+								];
+
+								function formatNumber(n) {
+									for (var i = 0; i < ranges.length; i++) {
+										if (n >= ranges[i].divider) {
+											return (n / ranges[i].divider).toString() + ranges[i].suffix;
+										}
+									}
+									return n;
+								}
+								return formatNumber(value);
+							}
+						},
+						gridLines: {
+							display: true,
+							drawBorder: false,
+							color: '#cccccc',
+							borderDash: [5, 5],
+							zeroLineBorderDash: [5, 5],
+						}
+					}]
+				},
+				legend: {
+					display: false
+				},
+				tooltips: {
+					mode: 'index',
+					intersect: false,
+					enabled: true
+				},
+				elements: {
+					line: {
+						tension: .35
+					},
+					point: {
+						pointStyle: 'circle'
+					}
+				}
+			};
+			var earningCanvas = $("#earning-line-chart").get(0).getContext("2d");
+			var earningChart = new Chart(earningCanvas, {
+				type: 'line',
+				data: lineChartData,
+				options: lineChartOptions
+			});
+		}
+
+		/*-------------------------------------
+			  Bar Chart 
+		  -------------------------------------*/
+		if ($("#expense-bar-chart").length) {
+
+			var barChartData = {
+				labels: ["Jan", "Feb", "Mar"],
+				datasets: [{
+					backgroundColor: ["#40dfcd", "#417dfc", "#ffaa01"],
+					data: [125000, 100000, 75000, 50000, 150000],
+					label: "Expenses (millions)"
+				},]
+			};
+			var barChartOptions = {
+				responsive: true,
+				maintainAspectRatio: false,
+				animation: {
+					duration: 2000
+				},
+				scales: {
+
+					xAxes: [{
+						display: false,
+						maxBarThickness: 100,
+						ticks: {
+							display: false,
+							padding: 0,
+							fontColor: "#646464",
+							fontSize: 14,
+						},
+						gridLines: {
+							display: true,
+							color: '#e1e1e1',
+						}
+					}],
+					yAxes: [{
+						display: true,
+						ticks: {
+							display: true,
+							autoSkip: false,
+							fontColor: "#646464",
+							fontSize: 14,
+							stepSize: 25000,
+							padding: 20,
+							beginAtZero: true,
+							callback: function (value) {
+								var ranges = [{
+									divider: 1e6,
+									suffix: 'M'
+								},
+								{
+									divider: 1e3,
+									suffix: 'k'
+								}
+								];
+
+								function formatNumber(n) {
+									for (var i = 0; i < ranges.length; i++) {
+										if (n >= ranges[i].divider) {
+											return (n / ranges[i].divider).toString() + ranges[i].suffix;
+										}
+									}
+									return n;
+								}
+								return formatNumber(value);
+							}
+						},
+						gridLines: {
+							display: true,
+							drawBorder: true,
+							color: '#e1e1e1',
+							zeroLineColor: '#e1e1e1'
+
+						}
+					}]
+				},
+				legend: {
+					display: false
+				},
+				tooltips: {
+					enabled: true
+				},
+				elements: {}
+			};
+			var expenseCanvas = $("#expense-bar-chart").get(0).getContext("2d");
+			var expenseChart = new Chart(expenseCanvas, {
+				type: 'bar',
+				data: barChartData,
+				options: barChartOptions
+			});
+		}
+
+		/*-------------------------------------
+			  Doughnut Chart 
+		  -------------------------------------*/
+		if ($("#student-doughnut-chart").length) {
+
+			var doughnutChartData = {
+				labels: ["Female Students", "Male Students"],
+				datasets: [{
+					backgroundColor: ["#304ffe", "#ffa601"],
+					data: [45000, 105000],
+					label: "Total Students"
+				},]
+			};
+			var doughnutChartOptions = {
+				responsive: true,
+				maintainAspectRatio: false,
+				cutoutPercentage: 65,
+				rotation: -9.4,
+				animation: {
+					duration: 2000
+				},
+				legend: {
+					display: false
+				},
+				tooltips: {
+					enabled: true
+				},
+			};
+			var studentCanvas = $("#student-doughnut-chart").get(0).getContext("2d");
+			var studentChart = new Chart(studentCanvas, {
+				type: 'doughnut',
+				data: doughnutChartData,
+				options: doughnutChartOptions
+			});
+		}
+
+
+
+
+
+		///
 
 		function swithtab(data, offsidebar) {
 			$('.content-tab').hide();
@@ -48,18 +317,12 @@ jQuery(document).ready(function () {
 		}
 
 
-	
+
 
 
 		$(document).on("click", function (e) {
-			if (!$(e.target).is(".sidebar") && !jQuery(e.target).is(".sidebar *") && !jQuery(e.target).is(".menu-icon *")) {
-				$(".sidebar").removeClass("active");
-				$('.sidebar .item-bar li .sub').slideUp(200);
-				$('.sidebar .item-bar .item').removeClass('show');
-				$('.s-navi .await').removeClass('active');
-			}
-			if (!$(e.target).is(".head-sub") && !jQuery(e.target).is(".head-sub *")) {
-				$(".head-sub").removeClass("show");
+			if (!$(e.target).is(".navbar .navbar-item") && !jQuery(e.target).is(".navbar .navbar-item *")) {
+				$('.navbar .navbar-item').removeClass('show')
 			}
 		});
 
@@ -83,20 +346,20 @@ jQuery(document).ready(function () {
 
 		//
 		if (jQuery("#imageUpload").length) {
-		document.getElementById('imageUpload').addEventListener('change', function (event) {
-			const file = event.target.files[0];
-			if (file) {
-				const reader = new FileReader();
-				reader.onload = function (e) {
-					const preview = document.getElementById('imagePreview');
-					preview.src = e.target.result;
-					preview.style.display = 'block';
-					$('.preview-container').addClass('hasimg');
-				};
-				reader.readAsDataURL(file);
-			}
-		});
-	}
+			document.getElementById('imageUpload').addEventListener('change', function (event) {
+				const file = event.target.files[0];
+				if (file) {
+					const reader = new FileReader();
+					reader.onload = function (e) {
+						const preview = document.getElementById('imagePreview');
+						preview.src = e.target.result;
+						preview.style.display = 'block';
+						$('.preview-container').addClass('hasimg');
+					};
+					reader.readAsDataURL(file);
+				}
+			});
+		}
 
 		//
 
